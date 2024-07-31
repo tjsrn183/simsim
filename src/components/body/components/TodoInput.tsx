6473ea6000bb889ebb7e54519680d968f1955599
hwang <tjsrn183@naver.com>
2024-07-31 20:41:07 +0900

    fix: 버튼타입명시, form태그 활용

--- src/components/body/components/TodoInput.tsx
+++ src/components/body/components/TodoInput.tsx
@@ -1,20 +1,33 @@
 import React, { useState } from 'react';
 import useTodoStore from '../../../store';
-export const TodoInput=() => {
-    const { todos, addTodo, removeTodo } = useTodoStore();
-  const [newTodo, setNewTodo] = useState<string>('');
-  const handleAddTodo = () => {
-    if (newTodo.trim()) {
-      addTodo(newTodo);
-      setNewTodo('');
-    }
-  };
+import { FormEvent} from 'react';
+export const TodoInput = () => {
+    const { addTodo } = useTodoStore();
+    const [newTodo, setNewTodo] = useState('');
+
+    const handleAddTodo = (event:FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (newTodo.trim()) {
+            addTodo(newTodo);
+            setNewTodo('');
+        }
+    };
+
     return (
-        <div className=" flex items-center">
-            <input type="text" value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-        placeholder="Add a new todo" className=" border-solid border-2 rounded-lg h-[50px] w-[500px]"/>
-            <button  onClick={handleAddTodo}className=" bg-yellow-600 h-[50px] w-[110px] rounded-xl -translate-x-3 text-white">Add</button>
-        </div>
-    )
-}
+        <form onSubmit={handleAddTodo} className="flex items-center">
+            <input
+                type="text"
+                value={newTodo}
+                onChange={(e) => setNewTodo(e.target.value)}
+                placeholder="일정을 입력하세요."
+                className="border-solid border-2 rounded-lg h-[50px] w-[500px]"
+            />
+            <button
+                type="submit"
+                className="bg-yellow-600 h-[50px] w-[110px] rounded-xl -translate-x-3 text-white"
+            >
+                Add
+            </button>
+        </form>
+    );
+};
